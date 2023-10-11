17233550f085235c7f40550143b5cbd016e124ee
AnvXprogs <usanvar2013@gamil.com>
2023-10-11 19:57:10 +0300

    vue style

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  .box{
+    width:40px;
+    height:40px;
+    background-color:red;
+  }
+</style>
